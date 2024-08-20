de95ddc9430d6cd9344d9d0772cd1c5753128a8d
AdvaitConty <66001714+contyadvait@users.noreply.github.com>
2024-08-20 10:50:29 +0800

    Update emails.py

--- emails.py
+++ emails.py
@@ -3,7 +3,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-import secrets
+import secret as secrets
 
 
 def send_email(subject, body, to_emails, attachment_path, attach=True):
